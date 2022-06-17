7b9da1af8763fb10221e38bbf26cbc1bdde42a20
yusamaru9 <106299497+yusamaru9@users.noreply.github.com>
2022-06-17 14:11:18 +0000

    [Finish] 4-18

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   
   root to: "homes#top"
   devise_for :users
-  resources :post_images, only: [:new, :create, :index, :show, :destroy, :edit]
+  
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resources :post_comments, only: [:create, :destroy]
+  end
+  
   resources :users, only: [:show, :edit, :update]
   
 # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
